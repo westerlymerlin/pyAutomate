1440c43a06fd9d056a52d7e4ec1a7c78918c36c7
Gary Twinn <gary@twinn.net>
2025-06-14 09:13:53 +0100

    1.0.5   Added a copyright block

--- document_from_code.py
+++ document_from_code.py
@@ -50,7 +50,7 @@ from pydoc_markdown.interfaces import Context
 from pydoc_markdown.contrib.loaders.python import PythonLoader
 from pydoc_markdown.contrib.renderers.markdown import MarkdownRenderer
 
-CODEBLOCK: str = '\n  \n-------\n#### Copyright (C) YYYY Gary Twinn  \n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\nGNU General Public License for more details.  \n  \nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.  \n  \n  ##### Author: Gary Twinn  \n  \n ----------------------------'
+CODEBLOCK: str = '\n  \n-------\n#### Copyright (C) YYYY Gary Twinn  \n\nThis program is free software: you can redistribute it and/or modify\nit under the terms of the GNU General Public License as published by\nthe Free Software Foundation, either version 3 of the License, or\n(at your option) any later version.\n\nThis program is distributed in the hope that it will be useful,\nbut WITHOUT ANY WARRANTY; without even the implied warranty of\nMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the\nGNU General Public License for more details.  \n  \nYou should have received a copy of the GNU General Public License\nalong with this program. If not, see <https://www.gnu.org/licenses/>.  \n  \n  ##### Author: Gary Twinn  \n  \n -------------\n  '
 
 
 
